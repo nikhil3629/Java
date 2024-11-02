2d55ba160038d566f41d5b0a1aa06e74fdaa0854
Satrasala Nikhil Kumar <126808578+nikhil3629@users.noreply.github.com>
2024-11-02 19:57:49 +0530

    Add files via upload

---
+++ Packages/Cpack/Cclass.java
@@ -0,0 +1,15 @@
+package Cpack;
+
+import Apack.Aclass;
+
+public class Cclass extends Aclass{
+    public void cde(){
+        System.out.println("C public");
+        super.ameth();
+        // Aclass ooa = new Aclass();
+        // ooa.abc();
+
+    }
+    
+}
+/*We can get Aclass pack by import aand we can also get Aclass by inheritence */
