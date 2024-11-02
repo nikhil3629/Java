341059f3c91ed9bbf1fc5dbe9d68958b12c93eaa
Satrasala Nikhil Kumar <126808578+nikhil3629@users.noreply.github.com>
2024-11-02 19:59:23 +0530

    Add files via upload

---
+++ Packages/Dpack/Dclass.java
@@ -0,0 +1,12 @@
+package Dpack;
+public class Dclass{
+    public void def(){
+        System.out.println("D called");
+    }
+}
+/*In this we are creating package here itself.
+ * javac -d . Dclass.java
+ * -d indicates directory.
+ * . Dclass for same folder.
+ * we can join 
+ */
